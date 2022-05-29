c6b27d0825ef71c50416f4e52fa9e329b854654e
arifemir <arifarslanturk@gmail.com>
2022-05-29 19:30:48 +0300

    user flow

---
+++ server/routes/user-routes/userRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+const middlewares = require('../../middlewares')
+const controllers = require('../../controllers')
+
+router.post('/')
+
+router.patch('/')
+
+router.get('/')
+
+module.exports = router
