52c11da37b90b78ffe488a746d68a870ba02cd65
oguzzsancaktar <oguztahasancaktar@gmail.com>
2022-09-02 22:23:15 +0300

    latest

--- app.js
+++ app.js
@@ -41,13 +41,13 @@ const main = async () => {
 
   app.use(express.static(path.join(__dirname, '/client/build')))
 
-  // app.get('*', (req, res) => {
-  //   res.sendFile(path.join(__dirname, '/client/build', 'index.html'))
-  // })
-
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '.', 'index.html'))
+    res.sendFile(path.join(__dirname, '/client/build', 'index.html'))
   })
+
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.join(__dirname, '.', 'index.html'))
+  // })
 
   app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
 }
