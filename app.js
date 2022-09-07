e5cca3d7c51e54a171e29407508c3a726d3812a3
oguzzsancaktar <oguztahasancaktar@gmail.com>
2022-09-07 22:43:11 +0300

    mig update

--- app.js
+++ app.js
@@ -45,9 +45,9 @@ const main = async () => {
 
   app.use(express.static(path.join(__dirname, '/client/build')))
 
-  // app.get('*', (req, res) => {
-  //   res.sendFile(path.join(__dirname, '/client/build', 'index.html'))
-  // })
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build', 'index.html'))
+  })
 
   app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
 }
