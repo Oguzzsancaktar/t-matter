226b47d3a63581b4fd4d722739b73a1e497a650d
oguzzsancaktar <oguztahasancaktar@gmail.com>
2022-09-11 15:54:55 +0300

    colors added

--- client/src/pages/customer-modal/customer-modal-tabs/finance-tabs/InvoiceTab.tsx
+++ client/src/pages/customer-modal/customer-modal-tabs/finance-tabs/InvoiceTab.tsx
@@ -1,14 +1,8 @@
 import React, { useState } from 'react'
-import { Button, Column, H1, JustifyBetweenRow, JustifyCenterColumn, JustifyCenterRow } from '@/components'
+import { JustifyBetweenRow, JustifyCenterColumn, JustifyCenterRow } from '@/components'
 import { Document, Page, pdfjs } from 'react-pdf'
 import colors from '@constants/colors'
-import {
-  AdditionalTimeDonut,
-  InvoicesBarChart,
-  InvoicesDonut,
-  NonBillableCircleProgress,
-  UnPaidInvoicesCircleProgress
-} from '@/pages'
+import { AdditionalTimeDonut, InvoicesDonut, NonBillableCircleProgress, UnPaidInvoicesCircleProgress } from '@/pages'
 import styled from 'styled-components'
 import { Invoice } from '@/models'
 const Bordered = styled.div<{ margin?: string; width?: string }>`
