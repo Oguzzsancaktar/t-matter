546f922fea9559d2857ea4dffaba1c617d54118d
arifemirarslanturk <arifarslanturk@gmail.com>
2022-09-15 00:21:59 +0300

    side drawer

--- client/src/components/show-history/ShowHistory.tsx
+++ client/src/components/show-history/ShowHistory.tsx
@@ -3,14 +3,14 @@ import { H1, ItemContainer, Row } from '@/components'
 import { CornerUpLeft } from 'react-feather'
 
 interface IProps {
-  onClick: () => void
+  onClick: (e: React.MouseEvent<Element, MouseEvent>) => void
 }
 
-const ShowHistory: React.FC<IProps> = ({ onClick }) => {
+const ShowHistory: React.FC<IProps> = ({ onClick, children }) => {
   return (
     <ItemContainer cursorType="pointer" width="fit-content" position="absolute" right="12px" onClick={onClick}>
       <H1 cursor="pointer" fontSize="11px">
-        <CornerUpLeft size={11} /> Show History
+        <CornerUpLeft size={11} /> {children}
       </H1>
     </ItemContainer>
   )
