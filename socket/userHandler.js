17a7bdeccaa11e9aa4f4db8934639592666a8159
arifemirarslanturk <arifarslanturk@gmail.com>
2022-10-19 20:49:43 +0300

    xxxx

--- socket/userHandler.js
+++ socket/userHandler.js
@@ -1,9 +1,7 @@
 class UserHandler {
-  onlineUsers = {}
   io = null
   socket = null
   room = null
-  userId = null
 
   constructor(io) {
     this.io = io
@@ -12,19 +10,18 @@ class UserHandler {
   setSocket(socket) {
     this.socket = socket
     this.room = socket.handshake.query.room
-    this.userId = socket.handshake.query.userId
   }
 
-  addUser = () => {
-    this.onlineUsers[this.userId] = true
+  addUser = async () => {
     this.socket.join(this.room)
-    this.io.in(this.room).emit('online', { onlineUsers: Object.keys(this.onlineUsers) })
+    const clients = await this.io.in(this.room).fetchSockets()
+    this.io.in(this.room).emit('online', { onlineUsers: clients.map(({ handshake }) => handshake.query.userId) })
   }
 
-  removeUser = () => {
-    delete this.onlineUsers[this.userId]
+  removeUser = async () => {
     this.socket.leave(this.room)
-    this.io.in(this.room).emit('online', { onlineUsers: Object.keys(this.onlineUsers) })
+    const clients = await this.io.in(this.room).fetchSockets()
+    this.io.in(this.room).emit('online', { onlineUsers: clients.map(({ handshake }) => handshake.query.userId) })
   }
 }
 
