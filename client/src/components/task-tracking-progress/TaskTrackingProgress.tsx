c192f01476a5a627cc489c4cf5b717dea6173533
arifemirarslanturk <arifarslanturk@gmail.com>
2022-10-26 19:43:27 +0300

    xxxx

--- client/src/components/task-tracking-progress/TaskTrackingProgress.tsx
+++ client/src/components/task-tracking-progress/TaskTrackingProgress.tsx
@@ -5,6 +5,7 @@ import {
   H1,
   ItemContainer,
   JustifyBetweenRow,
+  JustifyCenterRow,
   ProgressBar,
   Row,
   UserImage
@@ -52,42 +53,48 @@ const TaskTrackingProgress: React.FC<ITaskTrackingProgressProps> = ({ activeTask
   const progress = (workedTime / totalWorkTime) * 100
 
   return (
-    <ItemContainer onClick={handleClick} borderRadius="0.3rem">
+    <ItemContainer onClick={handleClick} borderRadius="0.3rem" margin="0 0 0.75rem 0">
       <JustifyBetweenRow>
-        <ItemContainer height="30px" width="30px" margin=" 0 0.5rem 0 0">
-          <Avatar
-            size="md"
-            pointer
-            src={activeTaskStep.user.profile_img}
-            text={activeTaskStep.user.firstname[0] + '' + activeTaskStep.user.lastname[0]}
-            bordered
-            stacked
-            zoomed
-            squared
-          />
-        </ItemContainer>
-        <Column>
-          <ItemContainer>
+        <Avatar
+          size="md"
+          pointer
+          src={activeTaskStep.user.profile_img}
+          text={activeTaskStep.user.firstname[0] + '' + activeTaskStep.user.lastname[0]}
+          bordered
+          stacked
+          zoomed
+          squared
+          style={{ marginRight: '0.5rem' }}
+        />
+        <div
+          style={{
+            height: 40,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            width: '100%',
+            justifyContent: 'center'
+          }}
+        >
+          <ItemContainer margin="0 0 0.2rem 0">
             <JustifyBetweenRow>
               <Row>
-                <H1 width="auto" color={colors.text.primary}>
+                <H1 fontFamily="Satoshi-Light" width="auto" color={colors.text.primary}>
                   {activeTaskStep.task.name}
                 </H1>
-                <H1 margin="0 0 0 0.25rem" color={colors.blue.primary} width="auto">
+                <H1 fontFamily="Satoshi-Light" margin="0 0 0 0.25rem" color={colors.blue.primary} width="auto">
                   {activeTaskStep.task.steps[activeTaskStep.activeTaskStep].category.name}
                 </H1>
               </Row>
-              <Row>
-                <H1 width="auto" color={colors.text.primary}>
-                  {secondsToHourMin(workedTime)} / {secondsToHourMin(totalWorkTime)}
-                </H1>
-              </Row>
+              <H1 fontFamily="Satoshi-Light" width="200px" textAlign="end" color={colors.text.primary}>
+                {secondsToHourMin(workedTime)} / {secondsToHourMin(totalWorkTime)}
+              </H1>
             </JustifyBetweenRow>
           </ItemContainer>
           <ItemContainer>
             <ProgressBar completionPercentage={progress} completionColor={colors.blue.primary} />
           </ItemContainer>
-        </Column>
+        </div>
       </JustifyBetweenRow>
     </ItemContainer>
   )
